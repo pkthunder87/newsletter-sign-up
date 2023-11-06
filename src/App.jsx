a32f951d7d88737065e8d85049423625a0cf8ecb
pkthunder87 <pkthunder87@gmail.com>
2023-11-06 11:15:55 -0800

    Feat: Add desktop illustration image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div className=" grid h-[60rem]  w-[95rem] justify-center rounded-3xl bg-frontend-white">
-      Newsletter Sign-up
+    <div className=" grid h-[60rem] w-[95rem] grid-cols-[50%_50%] items-center rounded-[28px] bg-frontend-white px-8 pt-0">
+      <div>Stay updated!</div>
+      <div className="grid">
+        <img
+          className="-mr-4 scale-95 justify-self-end"
+          src="./illustration-sign-up-desktop.svg"
+          alt="Charts illustration"
+        />
+      </div>
     </div>
   );
 }
