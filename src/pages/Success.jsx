db6769de7774234d0225f09ff235e659e6eb0618
pkthunder87 <pkthunder87@gmail.com>
2023-11-16 13:57:01 -0800

    Feat: Adjust mobile phone design for Success.jsx

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -9,7 +9,7 @@ function Success({ userEmail }) {
   }
 
   return (
-    <div className="flex h-[100dvh] w-[100dvw] flex-col bg-frontend-white p-10 leading-[1] md:h-[48dvh] md:w-[34dvw] md:gap-8 md:rounded-[32px] md:p-20 md:shadow-2xl md:shadow-dark-slate-grey">
+    <div className="flex h-[875px] w-[100dvw] flex-col bg-frontend-white p-10 leading-[1] md:h-[48dvh] md:w-[34dvw] md:gap-8 md:rounded-[32px] md:p-20 md:shadow-2xl md:shadow-dark-slate-grey">
       <img
         className="mt-52 h-[5.75rem] w-[5.75rem] md:-mt-4 md:h-[5.25rem] md:w-[5.25rem]"
         src="icon-success.svg"
